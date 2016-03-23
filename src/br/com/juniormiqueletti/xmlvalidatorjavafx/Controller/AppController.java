efd08a0e0001397f30b79ac02cd6fd7001a18d0f
Junior Miqueletti <juniormiqueletti@gmail.com>
2016-03-22 22:03:08 -0300

    First Adjust controller

--- src/br/com/juniormiqueletti/xmlvalidatorjavafx/Controller/AppController.java
+++ src/br/com/juniormiqueletti/xmlvalidatorjavafx/Controller/AppController.java
@@ -1,6 +1,8 @@
 package br.com.juniormiqueletti.xmlvalidatorjavafx.Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -28,4 +30,13 @@ public class AppController {
     @FXML
     private TextArea taXsdFile;
 
+    @FXML
+    public void validate(ActionEvent event){
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Information");
+        alert.setHeaderText("in progress..");
+        alert.show();
+    }
+    
 }

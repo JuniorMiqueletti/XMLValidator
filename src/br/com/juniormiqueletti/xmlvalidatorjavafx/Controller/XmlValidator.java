ef6be867647994daf2879e192878cb9205f6eef3
Junior Miqueletti <juniormiqueletti@gmail.com>
2016-04-13 21:39:22 -0300

    validanint all ocurrs

--- src/br/com/juniormiqueletti/xmlvalidatorjavafx/Controller/XmlValidator.java
+++ src/br/com/juniormiqueletti/xmlvalidatorjavafx/Controller/XmlValidator.java
@@ -1,56 +1,75 @@
 package br.com.juniormiqueletti.xmlvalidatorjavafx.Controller;
 
+import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
 import javax.xml.XMLConstants;
-import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 import java.io.File;
-import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by Junior-Pc on 24/03/2016.
+ * Class with validation method
  */
 public class XmlValidator {
 
     private File xmlFile;
     private File xsdFile;
-    private String parsedXmlFile;
-    private String parsedXsdFile;
-    JAXBContext jaxbContext;
-    Unmarshaller unmarshaller;
 
     public XmlValidator(File xmlFile, File xsdFile) {
         this.xmlFile = xmlFile;
         this.xsdFile = xsdFile;
     }
 
-    public String getParsedXmlFile() {
-        return parsedXmlFile;
-    }
-
-    public String getParsedXsdFile() {
-        return parsedXsdFile;
-    }
-
     public String validate() throws SAXException, JAXBException {
 
         try {
+            final List<SAXException> exceptions = new LinkedList<SAXException>();
+
             SchemaFactory factory =
                     SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
             Schema schema = factory.newSchema(xsdFile);
-            Validator validator = schema.newValidator();
+            final Validator validator = schema.newValidator();
+
+            validator.setErrorHandler(new ErrorHandler() {
+
+                @Override
+                public void warning(SAXParseException exception) throws SAXException {}
+
+                @Override
+                public void error(SAXParseException exception) throws SAXException {
+                    exceptions.add(exception);
+                }
+
+                @Override
+                public void fatalError(SAXParseException exception) throws SAXException {}
+            });
+
             validator.validate(new StreamSource(xmlFile));
-            return "Ok.";
-        } catch (IOException e){
+            String erros;
+            erros = "Validation..\n\n";
+
+            for (int i = 0; i < exceptions.size();i++){
+                erros = String.format("%s--\n%s\n", erros, exceptions.get(1+i).getMessage());
+                erros = String.format("%s  %s\n", erros, exceptions.get(i).getMessage());
+                i++;
+            }
+
+            if (exceptions.isEmpty()){
+                erros = String.format("%s%s",erros,"Ok !");
+            }
+
+            return erros;
+
+        } catch (Exception e){
             return e.getMessage();
-        }catch(SAXException e1){
-            return e1.getMessage();
         }
     }
 }
